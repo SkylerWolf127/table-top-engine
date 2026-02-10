f699ab2ae1aee35191eb3e2916ab8af15c447d77
SkylerWolf127 <156957978+SkylerWolf127@users.noreply.github.com>
2026-02-10 16:02:33 -0700

    Window struggles

--- TTE/src/Main.java
+++ TTE/src/Main.java
@@ -1,8 +1,33 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+import java.awt.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Diggity dungeons and all that! \n");
+
+        //Main Menu Window
+        JFrame frame = new JFrame("Table-Top-Engine | Alpha 0 | Main Menu");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.getContentPane().add(button, BorderLayout.CENTER);
+        //RENDER FRAME
+        frame.pack();
+        frame.setSize(800, 600);
+        frame.setBackground(Color.PINK);
+        frame.setVisible(true);
+
+
+        //Settings Window
+        JFrame settingsFrame = new JFrame("Table-Top-Engine | Alpha 0 | Settings");
+        frame.setSize(800, 600);
+        frame.pack();
+        frame.setBackground(Color.PINK);
+        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Create sheet window
+
+
 
     }
 }
