b30ef8646fb75e179670f6386c0898675745fa82
SkylerWolf127 <156957978+SkylerWolf127@users.noreply.github.com>
2026-04-07 15:55:09 -0600

    testing pushing stuff

--- TTE/src/Main.java
+++ TTE/src/Main.java
@@ -13,7 +13,7 @@ public class Main {
     private static final Color TEXT_DIM    = new Color(0x9A9AB0);
     private static final Color FIELD_BG    = new Color(0x0A0A1A);
     private static final Color BORDER_COL  = new Color(0x2A2A4A);
-
+    
     //main entry point to the program
     public static void main(String[] args) {
         JFrame frame = new JFrame("Table-Top-Engine | Main Menu | Alpha 1.0");
